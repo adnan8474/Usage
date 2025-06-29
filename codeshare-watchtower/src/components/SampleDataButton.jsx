49ec2857aa2ccddca2dca3472f52128e3c2ff8f4
adnan8474 <adnanalrefai@gmail.com>
2025-06-29 23:14:25 +0100

    Add Netlify config for SPA routing

---
+++ codeshare-watchtower/src/components/SampleDataButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { generateSampleData } from '../utils/sampleData.js';
+import { detectCollisions, detectUnusualPatterns, detectDeviceWardViolations } from '../utils/anomalyDetector.js';
+
+function SampleDataButton({ onFlags }) {
+  const loadSample = () => {
+    const data = generateSampleData();
+    const collisions = detectCollisions(data);
+    const unusual = detectUnusualPatterns(data);
+    const ward = detectDeviceWardViolations(data);
+    onFlags([...collisions, ...unusual, ...ward]);
+  };
+
+  return (
+    <button onClick={loadSample} className="px-3 py-2 bg-green-500 text-white rounded mr-2">
+      Load Sample
+    </button>
+  );
+}
+
+export default SampleDataButton;
